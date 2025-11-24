5365f0c0caf88ca4e7a6855b8dccd8634eb615c8
Moisés Esquivel <69405573+moisesEL@users.noreply.github.com>
2025-11-24 04:34:28 +0000

    renombre class

--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean, Integer, ForeignKey, DateTime
+from sqlalchemy import String, Boolean, Integer, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime, timezone
 from typing import List
@@ -7,6 +7,8 @@ from typing import List
 db = SQLAlchemy()
 
 ## Usuario ##
+
+
 class User(db.Model):
     __tablename__ = "users"
 
@@ -22,10 +24,12 @@ class User(db.Model):
    ## is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False) ##
 
     # relaciones
-    favorites_characters: Mapped[List["favorites_characters"]] = relationship("favorites_characters",
-                                                                              back_populates="user")
-    favorites_planets: Mapped[List["favorites_planets"]
-                              ] = relationship("favorites_planets", back_populates="user")
+    favorites_characters: Mapped[List["FavoritesCharacters"]] = relationship(
+        "FavoritesCharacters", back_populates="user"
+    )
+    favorites_planets: Mapped[List["FavoritesPlanets"]] = relationship(
+        "FavoritesPlanets", back_populates="user"
+    )
 
     def serialize(self):
         return {
@@ -40,7 +44,7 @@ class User(db.Model):
 
 
 ## Planeta ##
-class planets(db.Model):
+class Planets(db.Model):
     __tablename__ = "planets"
 
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -49,18 +53,21 @@ class planets(db.Model):
     population: Mapped[str] = mapped_column(String(10))
     climate: Mapped[str] = mapped_column(String(10))
     created_at: Mapped[datetime] = mapped_column(
-        default=lambda: datetime.now(timezone.utc))
+        default=lambda: datetime.now(timezone.utc)
+    )
 
     ## relaciones ##
-    favorites: Mapped[List["favorites_planets"]] = relationship(
-        "favorites_planets", back_populates="planet")
-    characters: Mapped[List["characters"]] = relationship(
-        "characters", back_populates="planet")
 
-
+    favorites: Mapped[List["FavoritesPlanets"]] = relationship(
+        "FavoritesPlanets", back_populates="planet"
+    )
+    characters: Mapped[List["Characters"]] = relationship(
+        "Characters", back_populates="planet"
+    )
 ## Personajes ##
 
-class characters(db.Model):
+
+class Characters(db.Model):
     __tablename__ = "characters"
 
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -68,44 +75,51 @@ class characters(db.Model):
     height: Mapped[int] = mapped_column(Integer)
     mass: Mapped[int] = mapped_column(Integer)
     birth_year: Mapped[str] = mapped_column(String)
-    home_world: Mapped[int] = mapped_column(ForeignKey(
-        "planets.id"))  # Clave foranea id de tabla planets
+    home_world: Mapped[int] = mapped_column(
+        # Clave foranea id de tabla planets
+        ForeignKey("planets.id"), nullable=True)
+
 
 # relaciones de personajes
-
-    planet: Mapped["planets"] = relationship(
-        "planets", back_populates="characters")
-    favorites: Mapped[List["favorites_characters"]] = relationship(
-        "favorites_characters", back_populates="character")
-
+    planet: Mapped["Planets"] = relationship(
+        "Planets", back_populates="characters"
+    )
+    favorites: Mapped[List["FavoritesCharacters"]] = relationship(
+        "FavoritesCharacters", back_populates="character"
+    )
 
 ## tabla favorites_characters ##
 
-class favorites_characters(db.Model):
+
+class FavoritesCharacters(db.Model):
     __tablename__ = "favorites_characters"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     character_id: Mapped[int] = mapped_column(ForeignKey("characters.id"))
 
-# relaciones inversa
+    # Relaciones inversas
     user: Mapped["User"] = relationship(
-        "users", back_populates="favorites_characters")
-    character: Mapped["characters"] = relationship(
-        "characters", back_populates="favorites")
+        "User", back_populates="favorites_characters"
+    )
+    character: Mapped["Characters"] = relationship(
+        "Characters", back_populates="favorites"
+    )
 
 
 ## tabla favorites_planets ##
 
-class favorites_planets(db.Model):
+class FavoritesPlanets(db.Model):
     __tablename__ = "favorites_planets"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     planet_id: Mapped[int] = mapped_column(ForeignKey("planets.id"))
 
-# relaciones inversa
+    # Relaciones inversas
     user: Mapped["User"] = relationship(
-        "users", back_populates="favorites_planets")
-    planet: Mapped["planets"] = relationship(
-        "planets", back_populates="favorites")
+        "User", back_populates="favorites_planets"
+    )
+    planet: Mapped["Planets"] = relationship(
+        "Planets", back_populates="favorites"
+    )
